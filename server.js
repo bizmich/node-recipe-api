0a808ae662bef9038bec4c2b65a9a73025fec447
Abdullo Abdumamadov <abdullo_a@hotmail.com>
2023-05-20 00:17:57 +0500

    working with dirnames for images

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
+
 require("dotenv").config();
 const recipeRoute = require("./routes/recipe-route");
 const commentRoute = require("./routes/comment-route");
@@ -12,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(__dirname + "uploads"));
 
 mongoose
   .connect(process.env.MONGO_URI)
