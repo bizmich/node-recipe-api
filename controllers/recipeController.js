272d27abd17971303f4de93d8b6f5991969a7d09
Abdullo Abdumamadov <abdullo_a@hotmail.com>
2023-05-19 18:15:26 +0500

    filtering data to sending

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,21 +1,42 @@
 const RecipeModel = require("../model/recipeModel");
 
 module.exports.getAllRecipe = async (req, res) => {
-  const recipe = await RecipeModel.find();
-
-  res.status(200).json({
-    list: recipe,
-  });
+  await RecipeModel.find()
+    .then((doc) => {
+      const filteredRecipes = doc.map((el) => {
+        return {
+          createdDate: el.createdDate,
+          description: el.description,
+          image: el.image,
+          ingredient: el.ingredient,
+          name: el.name,
+          cookingTime: el.cookingTime,
+          rate: el.rate,
+          id: el._id,
+        };
+      });
+      res.status(200).json({
+        list: filteredRecipes,
+        count: doc.length,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Произошла ошибка",
+        error: error,
+      });
+    });
 };
+
 module.exports.createRecipe = async (req, res) => {
   const recipeData = {
     createdDate: new Date().toISOString(),
-    description: "Some description here",
+    description: req.body.description,
     image: "image",
-    ingredient: ["asdf", "asdf", "asdf"],
-    name: "Pizzza",
-    cookingTime: 5,
-    rate: 5,
+    ingredient: req.body.ingredient,
+    name: req.body.name,
+    cookingTime: req.body.cookingTime,
+    rate: req.body.rate,
   };
 
   await RecipeModel.create(recipeData)
