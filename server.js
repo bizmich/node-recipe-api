466e51dd5e09fd1d31d074b094fb66bcfefdd30d
Abdullo Abdumamadov <abdullo_a@hotmail.com>
2023-05-19 16:50:35 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Recipe app listening on port ${port}!`));
